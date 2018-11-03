ae68aeb9fcf6b39ce6a5973479679afd9775786d
Salvador Guzman <guzmansalv@gmail.com>
2018-11-03 15:06:13 -0700

    working server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+extern crate actix_web;
+
+fn f(_: actix_web::Path<()>) -> impl actix_web::Responder {
+    println!("Hello");
+    return format!("<h1>Hello</h1>\n");
+}
+
 fn main() {
-    println!("Hello, world!");
+    fn factory() -> actix_web::App {
+        let path: &str = "/";
+        let method: actix_web::http::Method = actix_web::http::Method::GET;
+        return actix_web::App::new()
+        .route(path, method, f);
+    }
+
+    let addr: &str = "127.0.0.1:8080";
+    actix_web::server::new(factory)
+        .bind(addr).unwrap()
+        .run();
 }
