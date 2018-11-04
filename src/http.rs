1bae4535cf0af12c9a4dace7d9ea5aaf6a0407c6
Salvador Guzman <guzmansalv@gmail.com>
2018-11-03 19:37:48 -0700

    http.rs

---
+++ src/http.rs
@@ -0,0 +1,22 @@
+fn f(_: actix_web::Path<()>) -> impl actix_web::Responder {
+    println!("Hello");
+    return format!("<h1>Hello</h1>\n");
+}
+
+fn factory() -> actix_web::App {
+    let path: &str = "/";
+    let method: actix_web::http::Method = actix_web::http::Method::GET;
+
+    return actix_web::App::new()
+        .route(path, method, f);
+}
+
+pub fn server_init() {
+    let host: String = env("HOST", "0.0.0.0");
+    let port: String = env("PORT", "8080");
+    let addr: String = format!("{}:{}", host, port);
+
+    actix_web::server::new(factory)
+        .bind(addr).unwrap()
+        .run();
+}
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,6 @@
 extern crate actix_web;
 
-fn f(_: actix_web::Path<()>) -> impl actix_web::Responder {
-    println!("Hello");
-    return format!("<h1>Hello</h1>\n");
-}
-
-fn factory() -> actix_web::App {
-    let path: &str = "/";
-    let method: actix_web::http::Method = actix_web::http::Method::GET;
-
-    return actix_web::App::new()
-        .route(path, method, f);
-}
+mod http;
 
 fn env(key: &str, default: &str) -> String {
     return match std::env::var(key) {
@@ -20,18 +9,8 @@ fn env(key: &str, default: &str) -> String {
     };
 }
 
-fn server_init() {
-    let host: String = env("HOST", "0.0.0.0");
-    let port: String = env("PORT", "8080");
-    let addr: String = format!("{}:{}", host, port);
-
-    actix_web::server::new(factory)
-        .bind(addr).unwrap()
-        .run();
-}
-
 fn main() {
     println!("start");
 
-    server_init();
+    http::server_init();
 }
