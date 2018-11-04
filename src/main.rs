2775f928b817e6aa47b4684bc6ae0f48f68677cb
Salvador Guzman <guzmansalv@gmail.com>
2018-11-03 19:19:58 -0700

    env vars

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,33 @@ fn f(_: actix_web::Path<()>) -> impl actix_web::Responder {
     return format!("<h1>Hello</h1>\n");
 }
 
-fn main() {
-    fn factory() -> actix_web::App {
-        let path: &str = "/";
-        let method: actix_web::http::Method = actix_web::http::Method::GET;
-        return actix_web::App::new()
-        .route(path, method, f);
-    }
+fn factory() -> actix_web::App {
+    let path: &str = "/";
+    let method: actix_web::http::Method = actix_web::http::Method::GET;
 
-    let addr: &str = "127.0.0.1:8080";
+    return actix_web::App::new()
+        .route(path, method, f);
+}
+
+fn env(key: &str, default: &str) -> String {
+    return match std::env::var(key) {
+        Ok(lang) => lang,
+        Err(e) => format!("{}", default)
+    };
+}
+
+fn server_init() {
+    let host: String = env("HOST", "0.0.0.0");
+    let port: String = env("PORT", "8080");
+    let addr: String = format!("{}:{}", host, port);
+
     actix_web::server::new(factory)
         .bind(addr).unwrap()
         .run();
+}
+
+fn main() {
+    println!("start");
+
+    server_init();
 }
